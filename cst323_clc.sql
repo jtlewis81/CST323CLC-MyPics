714f83b7138d263165a2e765c64d590ec53d66e5
jtlewis81 <jtlewis91481@gmail.com>
2023-01-29 14:47:19 -0500

    updated project for AWS

--- cst323_clc.sql
+++ cst323_clc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 22, 2023 at 01:14 AM
+-- Generation Time: Jan 29, 2023 at 07:07 PM
 -- Server version: 5.7.24
 -- PHP Version: 8.1.0
 
@@ -35,13 +35,6 @@ CREATE TABLE `images` (
   `User_ID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `images`
---
-
-INSERT INTO `images` (`ID`, `Description`, `File`, `Timestamp`, `User_ID`) VALUES
-(39, 'JT Shield Logo 500x500', 'JT Shield Logo 500x500.png', '01-21-2023, 08:06:33 PM', 1026);
-
 -- --------------------------------------------------------
 
 --
@@ -57,13 +50,6 @@ CREATE TABLE `users` (
   `Password` varchar(64) NOT NULL,
   `ProfilePicture` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`ID`, `FirstName`, `LastName`, `Email`, `Username`, `Password`, `ProfilePicture`) VALUES
-(1026, 'Jamie', 'Lewis', 'jlewis159@my.gcu.edu', 'jlewis', '$2a$10$n5V9/AxLbPBKIaln.j7y7O3uPUyKcprc86PLmKFazWFndEUbJGui6', NULL);
 
 --
 -- Indexes for dumped tables
@@ -89,13 +75,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1027;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
